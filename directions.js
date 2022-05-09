8ccdce4d027f2547b237d30aa3cb4a947baf1fcf
gibson26 <gibson26@miamioh.edu>
2022-05-08 21:23:58 -0400

    Update directions.js

--- directions.js
+++ directions.js
@@ -1,21 +1,50 @@
 //Jordan Gibson
-var key="ZJA9lprL9oQaFzq4Mj0he4dMlXvG71El"
-var fuzzyURL="https://api.tomtom.com/search/2/search/"
-var fuzzyURL2=".json?minFuzzyLevel=1&maxFuzzyLevel=2&view=Unified&relatedPois=off&key=" + key;
-var routeURL="https://https://api.tomtom.com/routing/1/calculateRoute/";
-$( document ).ready(function() {
-	
-});
+var key = "ZJA9lprL9oQaFzq4Mj0he4dMlXvG71El"
+var fuzzyURL = "https://api.tomtom.com/search/2/search/"
+var fuzzyURL2 = ".json?minFuzzyLevel=1&maxFuzzyLevel=2&view=Unified&relatedPois=off&key=" + key;
+var routeURL = "https://api.tomtom.com/routing/1/calculateRoute/";
+var lat1 = 0;
+var lon1 = 0;
+var lat2 = 0;
+var lon2 = 0;
+$(document).ready(function() {});
 
-function getFuzzy() {
-	var input = $("#startingAddress").val()
-	a=$.ajax({
-		url: fuzzyURL + encodeURIComponent(input) + fuzzyURL2,
+function getRoute() {
+	getFuzzyStart();
+	getFuzzyEnd();
+	var routeType = $("input[name='routeType']:checked").val();
+	var travelMode = $("input[name='travelMode']:checked").val();
+	var hilliness = $("input[name='hilliness']:checked").val();
+	a = $.ajax({
+		url: routeURL + encodeURIComponent(lat1 + "," + lon1 + ":" + lat2 + "," + lon2) + "/json?routeType=" + routeType + "&travelMode=" + travelMode + "&hilliness=" + hilliness + "&key=" + key,
 		method: "GET"
 	}).done(function(data) {
-		alert(data.results[0].position.lon);
-		alert(data.results[0].position.lat);
-	}).fail(function(error) {
-		console.log("error",error.statusText);
-	});
-}
+			console.log("Routes", data.routes[0]);
+			}).fail(function(error) {
+			console.log("error", error.statusText);
+		});
+	}
+
+	function getFuzzyStart() {
+		b = $.ajax({
+			url: fuzzyURL + encodeURIComponent($("#startingAddress").val()) + fuzzyURL2,
+			method: "GET"
+		}).done(function(data) {
+			lat1 = data.results[0].position.lat;
+			lon1 = data.results[0].position.lon;
+		}).fail(function(error) {
+			console.log("error", error.statusText);
+		});
+	}
+
+	function getFuzzyEnd() {
+		c = $.ajax({
+			url: fuzzyURL + encodeURIComponent($("#endingAddress").val()) + fuzzyURL2,
+			method: "GET"
+		}).done(function(data) {
+			lat2 = data.results[0].position.lat;
+			lon2 = data.results[0].position.lon;
+		}).fail(function(error) {
+			console.log("error", error.statusText);
+		});
+	}
