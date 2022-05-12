837599a8dd169ace0fcff62b180dc0fae15113ba
gibson26 <gibson26@miamioh.edu>
2022-05-12 08:39:50 -0400

    Update directions.js

--- directions.js
+++ directions.js
@@ -33,13 +33,11 @@ function getRoute() {
 		// Delay to make sure we recieve fuzzy latitudes and longitudes before continuing
 		setTimeout(function() {
 			getRoute2();
-			}, 500);
+		}, 500);
 	}
 }
 
 function getRoute2() {
-	//300 Roselake Dr Centerville OH
-	//1650 Magnolia Dr Oxford OH
 	var routeType = $("input[name='routeType']:checked").val();
 	var travelMode = $("input[name='travelMode']:checked").val();
 	var hilliness = $("input[name='hilliness']:checked").val();
@@ -51,7 +49,7 @@ function getRoute2() {
 	a = $.ajax({
 		url: URLfinal,
 		method: "GET"
-	}).done(function (data) {
+	}).done(function(data) {
 		$("#route").append("<h2 class='row justify-content-center'>Direction For Your Route</h2>");
 		$("#route").append("<p class='row justify-content-center'>" + "Miles: " + (data.routes[0].summary.lengthInMeters * 0.000621371) + " Miles</li>");
 		$("#route").append("<p class='row justify-content-center'>" + "Minutes Until Arrival: " + (data.routes[0].summary.travelTimeInSeconds / 60) + " Minutes</li>");
@@ -59,13 +57,13 @@ function getRoute2() {
 		for (let i = 0; i <= data.routes[0].guidance.instructions.length - 1; i++) {
 			$("#route").append("<p class='row justify-content-center'>" + data.routes[0].guidance.instructions[i].message + ".</li>");
 			var mapURL2 = mapURL + encodeURIComponent(data.routes[0].guidance.instructions[i].point.longitude) + "," + encodeURIComponent(data.routes[0].guidance.instructions[i].point.latitude) + "&width=512&height=512&view=Unified&key=" + key;
-            var map = document.createElement("img");
-            map.src = mapURL2;
-            $("#route").append(map);
+			var map = document.createElement("img");
+			map.src = mapURL2;
+			$("#route").append(map);
 			$("#route").append("<br>");
 		}
 		$("#route").append("<br>");
-	}).fail(function (error) {
+	}).fail(function(error) {
 		console.log("Failure in GET Request Routing", error);
 		alert(error)
 	});
@@ -75,10 +73,10 @@ function getFuzzyStart() {
 	b = $.ajax({
 		url: fuzzyURL + encodeURIComponent($("#startingAddress").val()) + fuzzyURL2,
 		method: "GET"
-	}).done(function (data) {
+	}).done(function(data) {
 		lat1 = data.results[0].position.lat;
 		lon1 = data.results[0].position.lon;
-	}).fail(function (error) {
+	}).fail(function(error) {
 		console.log("error", error.statusText);
 		alert(error)
 	});
@@ -88,10 +86,10 @@ function getFuzzyEnd() {
 	c = $.ajax({
 		url: fuzzyURL + encodeURIComponent($("#endingAddress").val()) + fuzzyURL2,
 		method: "GET"
-	}).done(function (data) {
+	}).done(function(data) {
 		lat2 = data.results[0].position.lat;
 		lon2 = data.results[0].position.lon;
-	}).fail(function (error) {
+	}).fail(function(error) {
 		console.log("error", error.statusText);
 		alert(error)
 	});
